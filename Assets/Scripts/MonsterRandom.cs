ca262c068129dbaa07ead475fe935ddd864be756
personallen1028@gmail.com <personallen1028@gmail.com>
2024-03-29 16:21:08 +0900

    feat : Monster Random Spawn

---
+++ Assets/Scripts/MonsterRandom.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public GameObject Enemy;
+    public int xPos; // 생성되는 x 좌표
+    public int zPos; // 생성되는 y 좌표
+    public int enemyCount; // 생성되는 몬스터의 수
+
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 10)
+        {
+            xPos = Random.Range(1, 50);
+            zPos = Random.Range(1, 31);
+            Instantiate(Enemy, new Vector3(xPos, 1, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+}
+
