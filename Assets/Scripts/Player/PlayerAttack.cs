5a1d6ceb558d78ca18bf2663819fa0ee50345dbf
bbiyakbbiyak <wonwon1028@naver.com>
2024-03-30 19:39:49 +0900

    Monster Attak / hp Animation Event

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
+    [SerializeField] Weapon weapon;
+
     public Animator animator;
     private bool isAttacking = false; // 플레이어가 공격중인지 확인
+    public GameObject weaponObject;
 
     private void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            ActiveAttackAnimation();
-          
+            ActiveAttackAnimation();        
         }   
     }
     public void ActiveAttackAnimation()
@@ -22,19 +25,26 @@ public class PlayerAttack : MonoBehaviour
             if (!isAttacking) 
             {
                 animator.SetTrigger("Attack");
-
                 isAttacking = true;
-
                 StartCoroutine(WaitForAnimation());
             }
         }
-
     }
 
     private IEnumerator WaitForAnimation()
     {
         yield return new WaitForSeconds(1f); // 애니메이션 구현 중 (1)초 간은 다른 애니메이션 발생 x 
         isAttacking = false; // 재생 종료
+    }
+    
+    public void EnableWeapon()
+    {
+        weapon.EnableWeapon();
+    }
+
+    public void DisableWeapon()
+    {
+        weapon.DisableWeapon();
     }
 }
 
