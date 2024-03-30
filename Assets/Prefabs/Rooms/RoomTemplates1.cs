51a4a1b969037be314aa5360358fe0a3937b7963
Tae Hong Ahn <taetae@umich.edu>
2024-03-30 23:56:45 +0900

    feat : prototype 2 WIP

---
+++ Assets/Prefabs/Rooms/RoomTemplates1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates1 : MonoBehaviour
+{
+    // south means south is open
+    public GameObject startingRoom;
+    public GameObject longRoom;
+    public GameObject wideRoom;
+    public GameObject bigRoom;
+}
