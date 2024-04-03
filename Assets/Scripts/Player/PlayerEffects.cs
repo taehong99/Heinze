33f2e73cbe67589dd27400a7214d61be3a7977f6
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-04-03 12:48:57 +0900

    feat : basic attack effects

--- Assets/Scripts/Player/PlayerEffects.cs
+++ Assets/Scripts/Player/PlayerEffects.cs
@@ -9,12 +9,15 @@ public class PlayerEffects : MonoBehaviour
     [SerializeField] ParticleSystem SlashEffect;
     [SerializeField] ParticleSystem SlamEffect;
 
+    private PooledObject Pierce1;
+    private PooledObject Slash;
+    private PooledObject Slam;
+
     private void Start()
     {
-        PierceEffect1.Stop();
-        PierceEffect2.Stop();
-        SlashEffect.Stop();
-        SlamEffect.Stop();
+        Pierce1 = Manager.Resource.Load<PooledObject>("Effects/WarriorPierce1");
+        Slash = Manager.Resource.Load<PooledObject>("Effects/WarriorSlash");
+        Slam = Manager.Resource.Load<PooledObject>("Effects/WarriorSlam");
     }
 
     public void PlayEffect(string effect)
@@ -23,16 +26,16 @@ public class PlayerEffects : MonoBehaviour
         {
             case "Pierce1":
                 Debug.Log("Pricked");
-                PierceEffect1.Play(true);
+                Manager.Pool.GetPool(Pierce1, transform.position + Vector3.up * 0.8f, transform.rotation);
                 break;
             case "Pierce2":
                 PierceEffect2.Play(true);
                 break;
             case "Slash":
-                SlashEffect.Play(true);
+                Manager.Pool.GetPool(Slash, transform.position + Vector3.up * 0.8f, transform.rotation);
                 break;
             case "Slam":
-                SlamEffect.Play(true);
+                Manager.Pool.GetPool(Slam, transform.position, transform.rotation);
                 break;
             default:
                 break;
