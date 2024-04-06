5b380351b1cec0a0675c23a3aa3672b60a2d7dbe
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-04-06 20:34:35 +0900

    chore : namespace dupe class

--- Assets/Scripts/UI/Interactor.cs
+++ Assets/Scripts/UI/Interactor.cs
@@ -2,46 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactor : MonoBehaviour
+namespace Tae
 {
-    [SerializeField] LayerMask interactableMask;
-    private IInteractable curInteractable;
-
-    private void Start()
+    public class Interactor : MonoBehaviour
     {
-        curInteractable = null;
+        [SerializeField] LayerMask interactableMask;
+        private IInteractable curInteractable;
 
-        // subscribe to player interact input event
-        transform.GetComponentInParent<PlayerController>().InteractPressed += OnInteract;
-    }
-
-    private void OnInteract()
-    {
-        if (curInteractable == null)
-            return;
-
-        Debug.Log("Interact");
-        curInteractable.HideUI();
-        curInteractable.Interact();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        IInteractable interactable = other.GetComponent<IInteractable>();
-        if(interactable != null)
+        private void Start()
         {
-            curInteractable = interactable;
-            curInteractable.ShowUI();
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        IInteractable interactable = other.GetComponent<IInteractable>();
-        if (interactable != null)
-        {
-            curInteractable.HideUI();
             curInteractable = null;
+
+            // subscribe to player interact input event
+            transform.GetComponentInParent<PlayerController>().InteractPressed += OnInteract;
+        }
+
+        private void OnInteract()
+        {
+            if (curInteractable == null)
+                return;
+
+            Debug.Log("Interact");
+            curInteractable.HideUI();
+            curInteractable.Interact();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            IInteractable interactable = other.GetComponent<IInteractable>();
+            if (interactable != null)
+            {
+                curInteractable = interactable;
+                curInteractable.ShowUI();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            IInteractable interactable = other.GetComponent<IInteractable>();
+            if (interactable != null)
+            {
+                curInteractable.HideUI();
+                curInteractable = null;
+            }
         }
     }
 }
